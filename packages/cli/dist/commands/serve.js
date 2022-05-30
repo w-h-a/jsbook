053fa1088362ac6b6b081fdfc15cb70b925a9c70
Wes Anderson <wesley.herb.anderson@gmail.com>
2022-05-30 15:44:04 -0400

    adding basic error handling

--- packages/cli/dist/commands/serve.js
+++ packages/cli/dist/commands/serve.js
@@ -1,4 +1,13 @@
 "use strict";
+var __awaiter = (this && this.__awaiter) || function (thisArg, _arguments, P, generator) {
+    function adopt(value) { return value instanceof P ? value : new P(function (resolve) { resolve(value); }); }
+    return new (P || (P = Promise))(function (resolve, reject) {
+        function fulfilled(value) { try { step(generator.next(value)); } catch (e) { reject(e); } }
+        function rejected(value) { try { step(generator["throw"](value)); } catch (e) { reject(e); } }
+        function step(result) { result.done ? resolve(result.value) : adopt(result.value).then(fulfilled, rejected); }
+        step((generator = generator.apply(thisArg, _arguments || [])).next());
+    });
+};
 var __importDefault = (this && this.__importDefault) || function (mod) {
     return (mod && mod.__esModule) ? mod : { "default": mod };
 };
@@ -11,6 +20,15 @@ exports.serveCommand = new commander_1.Command()
     .command('serve [filename]')
     .description('open a file for editing')
     .option('-p, --port <number>', 'port to run the server on', '4005')
-    .action((filename = 'notebook.js', opts) => {
-    (0, local_api_1.serve)(parseInt(opts.port), path_1.default.basename(filename), path_1.default.join(process.cwd(), path_1.default.dirname(filename)));
-});
+    .action((filename = 'notebook.js', opts) => __awaiter(void 0, void 0, void 0, function* () {
+    try {
+        yield (0, local_api_1.serve)(parseInt(opts.port), path_1.default.basename(filename), path_1.default.join(process.cwd(), path_1.default.dirname(filename)));
+        console.log(`opened ${filename}. navigate to http://localhost:${opts.port} to edit.`);
+    }
+    catch (err) {
+        if (err instanceof Error) {
+            console.log('problem is that', err.message);
+        }
+        process.exit(1);
+    }
+}));
--- packages/local-api/dist/index.js
+++ packages/local-api/dist/index.js
@@ -1,9 +1,16 @@
 "use strict";
+var __importDefault = (this && this.__importDefault) || function (mod) {
+    return (mod && mod.__esModule) ? mod : { "default": mod };
+};
 Object.defineProperty(exports, "__esModule", { value: true });
 exports.serve = void 0;
+const express_1 = __importDefault(require("express"));
 const serve = (port, filename, dir) => {
-    console.log('serving traffic on port', port);
-    console.log('saving/fetching cells from', filename);
-    console.log('that file is in directory', dir);
+    const app = (0, express_1.default)();
+    return new Promise((resolve, reject) => {
+        app
+            .listen(port, resolve)
+            .on('error', reject);
+    });
 };
 exports.serve = serve;
